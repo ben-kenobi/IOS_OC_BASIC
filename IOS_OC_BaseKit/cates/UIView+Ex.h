94476acfa69a3e39c0d6c9c9c27b8388c1befa4d
yf <whatelsecani@gmail.com>
2019-02-14 10:36:10 +0800

    7.1

--- IOS_OC_BaseKit/cates/UIView+Ex.h
+++ IOS_OC_BaseKit/cates/UIView+Ex.h
@@ -8,8 +8,22 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-UIView * layoutView(UIView *sup,NSArray *subs,NSInteger colNum,BOOL full);
-UIView * layoutViewWithSize(UIView *sup,NSArray *subs,NSInteger colNum,BOOL full,CGSize size);
+    UIView *layoutViewWithSize(UIView *sup,NSArray *subs,NSInteger colNum,BOOL veritcalFull,CGSize size);
+    
+    UIView *layoutView(UIView *sup,NSArray *subs,NSInteger colNum,BOOL veritcalFull);
+    
+    /**
+     
+     @param sup layout所在的父view，可以是scrollview
+     @param subs 被layout的子views
+     @param colNum 排列的列数
+     @param veritcalFull 是否垂直方向铺满，即垂直间距动态拉长
+     @param sizeToFit 使用子view本身动态适配的size
+     @param horizonScrollable 是否水平方向可滑动，只在sup为scrollview时生效
+     @param horgap 水平方向间距
+     @return 返回最后一个子views
+     */
+    UIView *layoutViewWithParam(UIView *sup,NSArray *subs,NSInteger colNum,bool veritcalFull,bool sizeToFit,bool horizonScrollable,CGFloat horgap);
 #ifdef __cplusplus
 }
 #endif
